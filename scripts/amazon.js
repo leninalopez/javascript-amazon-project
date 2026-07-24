12f1d09277311f02eeea91325ffb78a269c2a0e0
LeninAlopez <lagustinlopez@icloud.com>
2026-07-23 23:19:33 -0400

    Load products from the backend

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,11 +1,14 @@
 import { cart, addToCart, calculateCartQuantity } from '../data/cart.js';
-import { products } from '../data/products.js';
+import { products, loadProducts } from '../data/products.js';
 import { formatCurrency } from './utils/money.js';
 
-let productsHTML = '';
+loadProducts(renderProductsGrid);
 
-products.forEach((product) => {
-  productsHTML += `
+function renderProductsGrid() {
+  let productsHTML = '';
+
+  products.forEach((product) => {
+    productsHTML += `
     <div class="product-container">
           <div class="product-image-container">
             <img class="product-image" src="${product.image}">
@@ -56,29 +59,30 @@ products.forEach((product) => {
           </button>
         </div>
   `;
-});
-
-document.querySelector('.js-products-grid').
-  innerHTML = productsHTML;
-
-function updateCartQuantity() {
-
-  const cartQuantity = calculateCartQuantity();
-
-
-  document.querySelector('.js-cart-quantity')
-    .innerHTML = cartQuantity;
-}
-
-
-updateCartQuantity();
-
-document.querySelectorAll('.js-add-to-cart')
-  .forEach((button) => {
-    button.addEventListener('click', () => {
-      const productId = button.dataset.productId;
-      addToCart(productId);
-      updateCartQuantity();
-
-    });
   });
+
+  document.querySelector('.js-products-grid').
+    innerHTML = productsHTML;
+
+  function updateCartQuantity() {
+
+    const cartQuantity = calculateCartQuantity();
+
+
+    document.querySelector('.js-cart-quantity')
+      .innerHTML = cartQuantity;
+  }
+
+
+  updateCartQuantity();
+
+  document.querySelectorAll('.js-add-to-cart')
+    .forEach((button) => {
+      button.addEventListener('click', () => {
+        const productId = button.dataset.productId;
+        addToCart(productId);
+        updateCartQuantity();
+
+      });
+    });
+}
